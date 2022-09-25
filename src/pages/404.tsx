69e05d46f9929babf072960c064ccb3d10fe2fdf
Anirudh Mitra <201951024@iiitvadodara.ac.in>
2022-09-25 23:03:53 +0530

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,47 @@
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="2xl"
+        bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={"gray.500"} mb={6}>
+        The page you're looking for does not seem to exist
+      </Text>
+
+      <Link href="/">
+        {/* <Button
+          colorScheme="teal"
+          bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+          color="white"
+          variant="solid"
+        >
+          Go to Home
+        </Button> */}
+
+        <Button
+          fontFamily={"heading"}
+          bgGradient="linear(to-r, red.400,pink.400)"
+          color={"white"}
+          _hover={{
+            bgGradient: "linear(to-r, red.400,pink.400)",
+            boxShadow: "xl",
+          }}
+        >
+          Go to Home
+        </Button>
+      </Link>
+    </Box>
+  );
+}
