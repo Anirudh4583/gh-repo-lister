8ad2861191820840ad5896fd2888b531e5e8ffbd
Anirudh Mitra <201951024@iiitvadodara.ac.in>
2022-09-25 23:02:25 +0530

    chore: next app init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import { AppProps } from "next/app";
+import { Hero } from "components/Hero";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Hero />
       <Component {...pageProps} />
+      <DarkModeSwitch />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
